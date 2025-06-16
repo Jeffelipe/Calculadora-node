7c35dca12a8196682908ad2a8ca388b01a789452
widleymayk <widleywmss@gmail.com>
2025-06-16 20:17:28 -0300

    funcao multiplicar

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
-function subtrair(expr) {
-  let numeros = expr.split('-').map(Number);
-  return numeros.reduce((acc, val) => acc - val);
+function multiplicar(expr) {
+  let numeros = expr.split('*').map(Number);
+  return numeros.reduce((acc, val) => acc * val, 1);
 }
-
