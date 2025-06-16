4a06b237f758507df4b9be78f7578a3d1c529a35
widleymayk <widleywmss@gmail.com>
2025-06-16 20:22:02 -0300

    Adicionando funcionalidade de subtração

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+function somar(expr) {
+  let numeros = expr.split('+').map(Number);
+  return numeros.reduce((acc, val) => acc + val, 0);
+}
+
+function subtrair(expr) {
+  let numeros = expr.split('-').map(Number);
+  return numeros.reduce((acc, val) => acc - val);
+}
 
 function multiplicar(expr) {
   let numeros = expr.split('*').map(Number);
