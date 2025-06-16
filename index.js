d32096821b6c5af470235300acaff5cfc2a26b54
widleymayk <widleywmss@gmail.com>
2025-06-16 20:17:00 -0300

    funcao subtrair

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function somar(expr) {
-  let numeros = expr.split('+').map(Number);
-  return numeros.reduce((acc, val) => acc + val, 0);
+
+function subtrair(expr) {
+  let numeros = expr.split('-').map(Number);
+  return numeros.reduce((acc, val) => acc - val);
 }
 
