447815743fdaa4a2b30b0e6daf5138ee296028c5
Jeffelipe <jfss16@aluno.ifal.edu.br>
2025-06-16 21:12:25 -0300

    check

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function dividir(expr) {
   let numeros = expr.split('/').map(Number);
   return numeros.reduce((acc, val) => acc / val);
 }
+function potenciar(expr) {
+  let numeros = expr.split('^').map(Number);
+  return numeros.reduce((acc, val) => Math.pow(acc, val));
+}
