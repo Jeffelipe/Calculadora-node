ac7bc145b0b56b8c07d543869d22175bc13f0b3b
Jeffelipe <jfss16@aluno.ifal.edu.br>
2025-06-16 21:15:36 -0300

    função media

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function potenciar(expr) {
   let numeros = expr.split('^').map(Number);
   return numeros.reduce((acc, val) => Math.pow(acc, val));
 }
+function media(expr) {
+  let numeros = expr.split('media').map(Number);
+  let soma = numeros.reduce((acc, val) => acc + val, 0);
+  return soma / numeros.length;
+}
